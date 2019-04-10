7d8f20e7261227f76f46ac5063b9c9bdb596be17
Henrik <hhs19942@gmail.com>
2019-04-10 12:40:37 +0200

    Remove loading from components. Change qoutes characters in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,26 @@
-import {NgModule} from "@angular/core";
-import {ReactiveFormsModule} from "@angular/forms";
-import {BrowserModule} from "@angular/platform-browser";
-import {AngularFireModule} from "@angular/fire";
-import {AngularFireAuthModule} from "@angular/fire/auth";
-import {AngularFirestoreModule} from "@angular/fire/firestore";
-import {AngularFireMessagingModule} from "@angular/fire/messaging";
-import {MarkdownModule} from "ngx-markdown";
+import {NgModule} from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {AngularFireModule} from '@angular/fire';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
+import {AngularFireMessagingModule} from '@angular/fire/messaging';
+import {MarkdownModule} from 'ngx-markdown';
 
-import {AppComponent} from "./app.component";
-import {AppRoutingModule} from "./app-routing.module";
-import {environment} from "../environments/environment";
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {environment} from '../environments/environment';
 
-import {ToastsComponent} from "./components/toasts/toasts.component";
-import {FooterComponent} from "./components/footer/footer.component";
-import {CourseListComponent} from "./components/course-list/course-list.component";
-import {CourseComponent} from "./components/course/course.component";
-import {StudentComponent} from "./components/student/student.component";
-import {TaComponent} from "./components/ta/ta.component";
-import {PostsComponent} from "./components/posts/posts.component";
-import { CourseLinkComponent } from './components/course-link/course-link.component';
-import { LoaderComponent } from './components/loader/loader.component';
-import { CourseBurgerBarComponent } from './components/course-burger-bar/course-burger-bar.component';
+import {ToastsComponent} from './components/toasts/toasts.component';
+import {FooterComponent} from './components/footer/footer.component';
+import {CourseListComponent} from './components/course-list/course-list.component';
+import {CourseComponent} from './components/course/course.component';
+import {StudentComponent} from './components/student/student.component';
+import {TaComponent} from './components/ta/ta.component';
+import {PostsComponent} from './components/posts/posts.component';
+import {CourseLinkComponent} from './components/course-link/course-link.component';
+import {LoaderComponent} from './components/loader/loader.component';
+import {CourseBurgerBarComponent} from './components/course-burger-bar/course-burger-bar.component';
 
 @NgModule({
   declarations: [
@@ -49,4 +49,4 @@ import { CourseBurgerBarComponent } from './components/course-burger-bar/course-
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
