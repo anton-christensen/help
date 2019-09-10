84e16a1172497a10b75453407fdade1cda543ade
Henrik <hhs19942@gmail.com>
2019-09-10 15:58:51 +0200

    Show offline message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from './services/auth.service';
+import {fromEvent, Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +8,21 @@ import {AuthService} from './services/auth.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  private online$: Observable<Event>;
+  private offline$: Observable<Event>;
+  public online = navigator.onLine;
+
   constructor(public auth: AuthService) {
   }
 
   ngOnInit(): void {
+    this.online$ = fromEvent(window, 'online');
+    this.offline$ = fromEvent(window, 'offline');
+
+    this.online$
+      .subscribe(() => this.online = true);
+
+    this.offline$
+      .subscribe(() => this.online = false);
   }
 }
