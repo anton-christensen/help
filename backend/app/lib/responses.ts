5a52117223dda1d6f4dffd88b59e3baa943b4435
Anton Christensen <anton.christensen9700@gmail.com>
2019-10-01 09:25:16 +0200

    bugfixes

--- backend/app/lib/responses.ts
+++ backend/app/lib/responses.ts
@@ -10,6 +10,17 @@ export const schemaErrorHandler: RequestHandler = (request, response, next) => {
 }
 
 export class HelpResponse {
+    static jsonWrap(payload: any) {
+        return JSON.stringify(payload) + '\n';
+    }
+
+    static successWrap(payload: any, success = true): any {
+        return this.jsonWrap({
+                success: success,
+                data: payload
+            });
+    }
+
     static fromPromise(response: Response, promise: Promise<any>) {
         promise
         .then( result => 
@@ -24,20 +35,10 @@ export class HelpResponse {
     };
 
     public static success(response: Response, payload: any) {
-        response.send(JSON.stringify(
-            {
-                success: true,
-                data: payload
-            }
-        ) + '\n');
+        response.send(this.successWrap(payload));
     }
     
     public static error(response: Response, error: any = "Internal server error", errorCode = 500) {
-        response.status(errorCode).send(JSON.stringify(
-            {
-                success: false,
-                data: error
-            }
-        ) + '\n');
+        response.status(errorCode).send(this.successWrap(error, false));
     };
 }
