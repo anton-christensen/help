d3ea9ad840cde8916bc0a3a6dabfff01a3fa6bac
Anton Christensen <anton.christensen9700@gmail.com>
2019-10-02 10:23:00 +0200

    merge and move readme

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -6,6 +6,6 @@ export const environment = {
     messagingSenderId: '9179952640',
     appId: '1:9179952640:web:d61aa7a7e86e1db1a54ae5'
   },
-  api: 'https://api.help.antonchristensen.net'
-  // api: 'http://192.168.0.51:7999'
+  api: 'http://localhost:7999'
+  // api: 'https://api.help.antonchristensen.net'
 };
